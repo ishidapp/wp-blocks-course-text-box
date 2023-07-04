9881c662093ec8826a8ab31d300a944377c0ce38
ko <kissy0913@yahoo.co.jp>
2023-07-04 19:56:02 +0900

    added for eslint error

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '9cef1b3bc25940590f76');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'dbb1b5af31a1cbb19d8a');
--- src/save.js
+++ src/save.js
@@ -1,24 +1,44 @@
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	getColorClassName,
+} from '@wordpress/block-editor';
+import classnames from 'classnames';
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {WPElement} Element to render.
- */
-export default function save() {
+export default function save({ attributes }) {
+	const {
+		text,
+		alignment,
+		backgroundColor,
+		textColor,
+		customBackgroundColor,
+		customTextColor,
+	} = attributes;
+
+	const backgroundClass = getColorClassName(
+		'background-color',
+		backgroundColor
+	);
+	const textClass = getColorClassName('color', textColor);
+
+	const classes = classnames(`text-box-align-${alignment}`, {
+		[textClass]: textClass,
+		[backgroundClass]: backgroundClass,
+	});
+
 	return (
-		<p { ...useBlockProps.save() }>
-			{ 'Boilerplate – hello from the saved content!' }
-		</p>
+		<RichText.Content
+			{...useBlockProps.save({
+				className: classes,
+				style: {
+					backgroundColor: backgroundClass
+						? undefined
+						: customBackgroundColor,
+					color: textClass ? undefined : customTextColor,
+				},
+			})}
+			tagName="h4"
+			value={text}
+		/>
 	);
 }
